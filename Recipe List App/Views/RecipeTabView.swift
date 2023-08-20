d5d97e6feb56ab936f04917c224dc19effc2ed87
Carlos Santos <gotgoodjob@gmail.com>
2023-08-20 10:41:50 +0800

    Add RecipeTabView to project

---
+++ Recipe List App/Views/RecipeTabView.swift
@@ -0,0 +1,32 @@
+//
+//  RecipeTabView.swift
+//  Recipe List App
+//
+//  Created by Carlos Dos santos on 20/08/2023.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        @State var tabIndex = 0
+        TabView (selection: $tabIndex) {
+            Text("Featured Recipe")
+                .tabItem {
+                    Image(systemName: "star.fill")
+                    Text("Featured")
+                }.tag(0)
+            RecipeListView()
+                .tabItem {
+                    Image(systemName: "list.bullet")
+                    Text("Others")
+                }.tag(1)
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
